2262cb52b61bd5e42c8b0e1966bc17b8fe4bd636
Estrella7323 <73238465@ucsm.edu.pe>
2021-07-13 14:59:14 -0500

    Actualizacion de comentario

--- app/src/androidTest/java/com/example/nomasfilas/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nomasfilas/ExampleInstrumentedTest.java
@@ -13,13 +13,13 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="http://d.android.com/tools/testing">Documentacion de Testing</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        .
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.nomasfilas", appContext.getPackageName());
     }
